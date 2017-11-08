3cce6d7541210093d388b423e296220db21eb91c
rprasad15 <rprasad15@studentmail.nait.ca>
2017-11-08 13:33:45 -0700

    Stuff

---
+++ ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using Chinook.Data.POCOs;
+using ChinookSystem.DAL;
+using System.ComponentModel;  //expose methods for ODS wizard
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class ArtistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Artist> Artists_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<SelectionList> List_ArtistNames()
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Artists
+                              orderby x.Name
+                              select new SelectionList
+                              {
+                                  IDValueField = x.ArtistId,
+                                  DisplayText = x.Name
+                              };
+                return results.ToList();
+            }
+        }
+    }
+}
---
+++ Website/Global.asax
@@ -0,0 +1,22 @@
+﻿<%@ Application Language="C#" %>
+<%@ Import Namespace="Website" %>
+<%@ Import Namespace="System.Web.Optimization" %>
+<%@ Import Namespace="System.Web.Routing" %>
+<%@ Import Namespace="ChinookSystem.BLL.Security" %>
+
+<script runat="server">
+
+    void Application_Start(object sender, EventArgs e)
+    {
+        RouteConfig.RegisterRoutes(RouteTable.Routes);
+        BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        var RoleManager = new RoleManager();
+        RoleManager.AddDefaultRoles();
+
+        var UserManager = new UserManager();
+        UserManager.AddWebMaster();
+        UserManager.AddEmployees();
+    }
+
+</script>
